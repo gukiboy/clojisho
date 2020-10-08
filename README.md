c5188143248b9ffa08f47bd9cdaf5eb78ad10742
Gustavo Kishima <gustavo.kishima@paygo.com.br>
2020-10-08 14:15:53 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# clojisho
+
+A Clojure wrapper for the jisho.org site API.
+
+## Usage
+
+
+    $ java -jar clojisho-0.1.0-standalone.jar [args]
+
---
+++ src/clojisho/core.clj
@@ -0,0 +1,21 @@
+(ns clojisho.core
+  (:require [clj-http.client :as client])
+  (:gen-class))
+
+(def base-url "http://jisho.org/api/v1")
+
+
+(defn search-keyword
+  [word]
+  (let [raw-response (client/get (str base-url "/search/words?keyword=" word) 
+                                 {:as :json})
+        body (:body raw-response)]
+    (:body raw-response)))
+
+(client/get (str base-url "/search/words?keyword=" "egg") 
+                                 {:as :clojure})
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
