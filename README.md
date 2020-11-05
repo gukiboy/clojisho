075c1627fc14edfba90a03553da6650bd4925ffd
Gustavo Kishima <gustavo.kishima@paygo.com.br>
2020-11-05 15:57:27 -0300

    Built in clojars. Removes junk and increases info on readme

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ A Clojure wrapper for the jisho.org site API.
 
 ## Usage
 
+### Leiningen/Boot
+`[clojisho "0.1.0-SNAPSHOT"]`
 
-    $ java -jar clojisho-0.1.0-standalone.jar [args]
+### Clojure CLI/deps.edn
+`clojisho {:mvn/version "0.1.0-SNAPSHOT"}`
+
+Use the function `(core.search-keyword "word")`, which will return the body of a request
+done to the API.
+
+https://clojars.org/clojisho
 
--- src/clojisho/core.clj
+++ src/clojisho/core.clj
@@ -4,17 +4,9 @@
 
 (def base-url "http://jisho.org/api/v1")
 
-
 (defn search-keyword
   [word]
   (let [raw-response (client/get (str base-url "/search/words?keyword=" word) 
                                  {:as :json})
         body (:body raw-response)]
     (:body raw-response)))
-
-(search-keyword "egg")
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
